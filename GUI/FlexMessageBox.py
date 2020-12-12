3c20696b638dc2a41ed16fd564d93dbcc1cdc760
Chun Yuen Tsang <tsangc@steelhead.nscl.msu.edu>
2020-12-11 23:50:15 -0500

    Added missing file

---
+++ GUI/FlexMessageBox.py
@@ -0,0 +1,37 @@
+import wx
+from wx.lib import sized_controls
+
+class FlexMessageBox(wx.Dialog):#sized_controls.SizedDialog):
+
+    def __init__(self, message, *args, **kwargs):
+        super(FlexMessageBox, self).__init__(*args, **kwargs)
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        panel = wx.Panel(self)
+       
+        lbl = wx.StaticText(panel, -1, style = wx.ALIGN_LEFT) 
+        font = wx.Font(12, wx.FONTFAMILY_TELETYPE, wx.NORMAL, wx.NORMAL, faceName="Monospace")
+        lbl.SetFont(font)
+        lbl.SetLabel(message)
+        sizer.Add(lbl, 0, wx.EXPAND | wx.ALL, 25)
+
+        button_ok = wx.Button(panel, wx.ID_OK, label='OK')
+        button_ok.Bind(wx.EVT_BUTTON, self.on_button)
+        sizer.Add(button_ok, 0, wx.ALIGN_RIGHT, 5)
+        self.Bind(wx.EVT_CLOSE, self.on_button)
+
+        panel.SetSizerAndFit(sizer)
+        self.Fit()
+
+    def on_button(self, event):
+        if self.IsModal():
+            self.EndModal(event.EventObject.Id)
+        else:
+            self.Close()
+
+
+if __name__ == '__main__':
+    app = wx.App(False)
+    dlg = FlexMessageBox('asdfasdfasdf\nasdfasdfasdf', None, title='Custom Dialog')
+    result = dlg.ShowModal()
+    dlg.Destroy()
+    app.MainLoop()
