e7e9a701265a7da5134a6c21c86e4987e1227703
tsangc <tommytsang3108@hotmail.com>
2021-04-23 12:09:25 -0400

    You can now select text from trace summary

--- GUI/FlexMessageBox.py
+++ GUI/FlexMessageBox.py
@@ -1,24 +1,33 @@
 import wx
 from wx.lib import sized_controls
+from wx.lib.expando import ExpandoTextCtrl, EVT_ETC_LAYOUT_NEEDED
 
 
-class FlexMessageBox(wx.Dialog):  # sized_controls.SizedDialog):
+class FlexMessageBox(sized_controls.SizedDialog):
 
     def __init__(self, message, *args, **kwargs):
         super(FlexMessageBox, self).__init__(*args, **kwargs)
         sizer = wx.BoxSizer(wx.VERTICAL)
         panel = wx.Panel(self)
 
-        lbl = wx.StaticText(panel, -1, style=wx.ALIGN_LEFT)
+        #self.lbl = wx.StaticText(panel, -1, style=wx.ALIGN_LEFT)
+        # find width required
         font = wx.Font(
             12,
             wx.FONTFAMILY_TELETYPE,
             wx.NORMAL,
             wx.NORMAL,
             faceName="Monospace")
-        lbl.SetFont(font)
-        lbl.SetLabel(message)
-        sizer.Add(lbl, 0, wx.EXPAND | wx.ALL, 25)
+        dc = wx.ScreenDC()
+        dc.SetFont(font)
+        w, h = dc.GetTextExtent(max(message.split('\n'), key=len))
+
+        margin = 15
+        self.lbl = ExpandoTextCtrl(panel, -1, style=wx.ALIGN_LEFT|wx.TE_READONLY|wx.NO_BORDER, size=(w + margin, -1))
+        self.lbl.SetBackgroundColour(panel.GetBackgroundColour())
+        self.lbl.SetFont(font)
+        sizer.Add(self.lbl, 1, wx.EXPAND | wx.ALL, 25)
+        self.lbl.WriteText(message)
 
         button_ok = wx.Button(panel, wx.ID_OK, label='OK')
         button_ok.Bind(wx.EVT_BUTTON, self.on_button)
@@ -31,7 +40,12 @@ class FlexMessageBox(wx.Dialog):  # sized_controls.SizedDialog):
         sizer.Add(hs)
         self.Bind(wx.EVT_CLOSE, self.on_close)
 
-        panel.SetSizerAndFit(sizer)
+        panel.SetSizer(sizer)
+        panel.Fit()
+
+        frameSizer = wx.BoxSizer()
+        frameSizer.Add(panel, 0, wx.EXPAND)
+        self.SetSizer(frameSizer)
         self.Fit()
 
     def on_button(self, event):
